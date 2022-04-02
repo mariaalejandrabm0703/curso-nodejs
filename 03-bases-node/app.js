9492253d51f04ebc25410772e87120f2e6810cb1
Maria Bohorquez <mariaalejandrabm0703@gmail.com>
2022-04-02 09:33:17 -0500

    configuraciones de yargs}

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,9 +1,27 @@
 const { crearArchivo } = require("./helpers/multiplicar");
+const argv = require("yargs")
+  .option("b", {
+    alias: "base",
+    type: "number",
+    demandOption: true,
+  })
+  .option("l", {
+    alias: "listar",
+    type: "boolean",
+    default: false,
+  })
+  .check((argv, options) => {
+    if (isNaN(argv.b)) {
+      throw "la base debe ser numero";
+    }
+    return true;
+  }).argv;
 
 console.clear();
 
-const base = 9;
+const base = argv.b;
+const listar = argv.l;
 
-crearArchivo(base)
+crearArchivo(base, listar)
   .then((nombre) => console.log(nombre, "creado"))
   .catch((err) => console.log(err));
--- 03-bases-node/helpers/multiplicar.js
+++ 03-bases-node/helpers/multiplicar.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const crearArchivo = async (base = 5, limite = 10) => {
+const crearArchivo = async (base = 5, listar, limite = 10) => {
   try {
     let salida = "";
 
@@ -11,8 +11,10 @@ const crearArchivo = async (base = 5, limite = 10) => {
     for (let i = 1; i <= limite; i++) {
       salida += `${base} * ${i} = ${base * i}\n`;
     }
-    console.log(salida);
-    fs.writeFileSync(`tabla${base}.txt`, salida);
+    if (listar) {
+      console.log(salida);
+    }
+    fs.writeFileSync(`tablas/tabla${base}.txt`, salida);
     return `tabla de ${base}`;
   } catch (err) {
     throw err;
