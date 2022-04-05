7638fff8aa9dc0a8a0ffa27c873f0a669329571a
Maria Bohorquez <mariaalejandrabm0703@gmail.com>
2022-04-04 19:35:56 -0500

    add repeat menu

--- 04-tareas-hacer/app.js
+++ 04-tareas-hacer/app.js
@@ -2,8 +2,39 @@ require("colors");
 const { mostrarMenu, pausa } = require("./helpers/mensajes");
 
 const main = async () => {
-  mostrarMenu();
-  pausa();
+  let opt = "";
+  do {
+    // Imprimir el menú
+    opt = await mostrarMenu();
+
+    switch (opt) {
+      case "1": // Crear tarea
+        console.log("1", 2);
+        break;
+
+      case "2": //Listar tareas
+        console.log("2", 2);
+        break;
+
+      case "3": // listar completadas
+        console.log("3", 3);
+        break;
+
+      case "4": // listar pendientes
+        console.log("4", 4);
+        break;
+
+      case "5": // completado | pendiente
+        console.log("5", 5);
+        break;
+
+      case "6": // Borrar
+        console.log("6", 6);
+        break;
+    }
+
+    await pausa();
+  } while (opt !== "0");
 };
 
 main();
